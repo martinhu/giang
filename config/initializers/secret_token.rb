fe43cfb036c50e713c8da392435a9100f2d8708e
Test <internetforsthausweg@gmail.com>
2011-04-30 17:37:21 +0200

    initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alina::Application.config.secret_token = '9feb1f1eb79cebd9bd337a3234bcc9898e7773d6fe2169e753d279ae828fbf9323e42abd422fcc06476ff6fd838c4aa1167327c63df2a2b66bb5c0bd80a5f960'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Alina::Application.config.session_store :cookie_store, :key => '_alina_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Alina::Application.config.session_store :active_record_store
